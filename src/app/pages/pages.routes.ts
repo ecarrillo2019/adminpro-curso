cb210cb459208106882fda3ea6db602694ce515e
Erik Carrillo <kire.mx@gmail.com>
2018-12-16 15:45:10 -0700

    Fin de la seccion 7

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,6 +4,7 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 const pagesRoutes: Routes = [
     {
@@ -12,7 +13,8 @@ const pagesRoutes: Routes = [
         children: [
             { path: 'dashboard', component: DashboardComponent },
             { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },            
+            { path: 'graficas1', component: Graficas1Component },
+            { path: 'accountsettings', component: AccountSettingsComponent },
             { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
         ]
     }
